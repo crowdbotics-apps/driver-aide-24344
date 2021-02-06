6b678e221b53d0bc1227b42b66e851c09325bdd8
Crowdbotics <team@crowdbotics.com>
2021-02-06 17:10:37 +0000

    Update models

--- backend/driver_aide_24344/wsgi.py
+++ backend/driver_aide_24344/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'driver_aide_24344.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "driver_aide_24344.settings")
 
 application = get_wsgi_application()
